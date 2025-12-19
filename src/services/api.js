0e9681e0710a036515d3b23ac094bd2cb6a88e0a
Firdavs Iskandarov <iskandarovfirdavs09@gmail.com>
2025-12-19 23:52:18 +0500

    some fixes for deploying

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 // API.js - API_BASE_URL ni to'g'ri o'rnating
-const API_BASE_URL = "";
+// const API_BASE_URL = "";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 class API {
   constructor() {
